f241ff0484691a687c12906dbf5e42531c831ddd
dhiaSaadi <saadidhia3@gmail.com>
2022-10-09 00:53:35 +0200

    swagger annotations + adjustment: it supposed to add this file for previous commit

--- src/main/java/de/mitigant/todo/controller/ToDoController.java
+++ src/main/java/de/mitigant/todo/controller/ToDoController.java
@@ -1,8 +1,14 @@
 package de.mitigant.todo.controller;
 
+import de.mitigant.todo.dto.ToDoRequest;
+import de.mitigant.todo.dto.ToDoResponse;
 import de.mitigant.todo.entity.ToDo;
 import de.mitigant.todo.service.ToDoService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +19,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+@OpenAPIDefinition(info = @Info(title = "TODO API", description = "TODO Information"))
 @RequestMapping(value = "/api/v1/item")
 public class ToDoController {
 
@@ -25,39 +33,42 @@ public class ToDoController {
 
     @PostMapping
     @ResponseBody
-    public ToDo create(@RequestBody ToDo item) {
+    public ToDoResponse create(@RequestBody ToDoRequest item) {
         return toDoService.addItem(item);
 
     }
 
     @PatchMapping("/{id}")
     @ResponseBody
-    public ToDo update(@PathVariable("id") UUID id, @RequestParam String description) {
+    public ToDoResponse update(@PathVariable("id") UUID id, @RequestParam String description) {
         return toDoService.updateDescription(id, description);
     }
 
     @GetMapping("/{id}")
     @ResponseBody
-    public ToDo getItem(@PathVariable("id") UUID id) {
+    public ToDoResponse getItem(@PathVariable("id") UUID id) {
         return toDoService.getSpecificItem(id);
     }
 
     @PatchMapping("/status/done/{id}")
     @ResponseBody
-    public ToDo markItemAsDone(@PathVariable("id") UUID id){
+    public ToDoResponse markItemAsDone(@PathVariable("id") UUID id){
         return toDoService.markItemAsStatusDone(id);
     }
 
     @PatchMapping("/status/notdone/{id}")
     @ResponseBody
-    public ToDo markItemAsNotDone(@PathVariable("id") UUID id){
-        return toDoService.markItemAsStatusNotDone(id);
+    public ToDoResponse markItemAsNotDone(@PathVariable("id") UUID id, @RequestParam("newduedate") @DateTimeFormat(iso =
+            DateTimeFormat.ISO.DATE_TIME) @Parameter(example="2022-10-08T20:45:59.002Z") LocalDateTime
+             newDueDate){
+        return toDoService.markItemAsStatusNotDone(id, newDueDate);
 
     }
 
     @GetMapping("/status/notdone")
     @ResponseBody
-    public List<ToDo> getAllItemsNotDone(@RequestParam Boolean option) {
+    public List<ToDo> getAllItemsNotDone(@RequestParam @Parameter(description="true : get items are not done, false: "
+            + "get all items") Boolean option) {
         if (option) {
             return toDoService.getAllItemsAreNotDone();
         } else {
