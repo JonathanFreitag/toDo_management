1de99c3cd13c436eb7e9be516900dde0f70ef0a4
dhiaSaadi <saadidhia3@gmail.com>
2022-10-24 22:12:01 +0200

    [feature] data migration with liquibase

---
+++ src/main/resources/db/changelog/v1/create_todo_table.sql
@@ -0,0 +1,9 @@
+create table todo
+(
+    id          uuid not null,
+    created_at  timestamp with time zone,
+    description varchar(256),
+    done_at     timestamp with time zone,
+    due_at      timestamp with time zone,
+    status      varchar(20)
+);git 
---
+++ src/main/resources/db/changelog/v1/initialize_todo_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO todo (id,
+                  created_at,
+                  description,
+                  done_at,
+                  due_at,
+                  status)
+VALUES ('ebc7f7a5-bc82-4cb0-ab11-83411c30d0c4',
+        '2022-10-24 21:39:11.861822',
+        'Youga',
+        null,
+        '2022-10-24 19:39:09.045',
+        'NOT_DONE');
